d8effd5031ea9e42ef02f8a1ea9183d94382e5f8
Anton Rechenauer <antonrechenauer@gmail.com>
2018-12-04 19:00:55 +0100

    param

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,11 @@ fn index() -> &'static str {
     "Hello, world - TA DB"
 }
 
+#[get("/num/<x>")]
+fn num(x:u64) -> String {
+    format!("Die Zahl ist {}",x)
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index,num]).launch();
 }
